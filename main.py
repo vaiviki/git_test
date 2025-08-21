10d0b3b814e43ac8d6a009438241d86a85e408ed
Virali <virali232@gmail.com>
2025-08-21 10:25:13 +0530

    changes added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import test
 data= [{"A":1,"B":2},{"A":1,"B":3}]
 
 df=pd.DataFrame(data)
 print(df)
+
+print(test.performMathOperation(5, 10))
--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("this is test")
+def performMathOperation(a, b):
+    """
+    This function performs a mathematical operation on two numbers.
+    It returns the sum of the two numbers.
+    """
+    return a + b
+
